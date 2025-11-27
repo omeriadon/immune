ccdc0ab59891367ca79ef95ffeed4bf626a231c6
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-27 22:56:59 +0800

    more adjustements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+        search={{
+        enabled: true
+        }}
+        >{children}</RootProvider>
       </body>
     </html>
   );
