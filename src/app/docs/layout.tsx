1cfae464ac37eaaf65335953c98b18ac944fafb7
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-27 22:37:57 +0800

    safe imporvements

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,23 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { baseOptions } from '@/lib/layout.shared';
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { baseOptions } from "@/lib/layout.shared";
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  );
+export default function Layout({ children }: LayoutProps<"/docs">) {
+	const base = baseOptions();
+
+	return (
+		<DocsLayout
+			tree={source.pageTree}
+			{...base}
+			githubUrl="https://github.com/omeriadon/immune"
+			nav={{
+				...base.nav,
+				enabled: true,
+				transparentMode: "always",
+				title: "Immune"
+			}}
+		>
+			{children}
+		</DocsLayout>
+	);
 }
