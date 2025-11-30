c952dff9f70fe37840b26a0648c1d8904283d8c2
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-11-30 14:14:23 +0800

    headigns look nice now

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,10 @@ import type { MDXComponents } from "mdx/types";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
+		h1: (props) => <h1 {...props} className="text-fd-primary" />,
+		h2: (props) => <h2 {...props} className="text-fd-primary" />,
+		h3: (props) => <h3 {...props} className="text-fd-primary" />,
+		h4: (props) => <h4 {...props} className="text-fd-primary" />,
 		...components,
 	};
 }
